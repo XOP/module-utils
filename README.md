2fc852adde5d559bc66ceca0bce1a1d85d9dcd5d
XOP <wdybih@gmail.com>
2015-12-02 12:56:58 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# module-utils
+# Module Utils
+
+[![npm version](https://badge.fury.io/js/xop-module-utils.svg)](https://www.npmjs.com/package/xop-module-utils)
+
 Useful helpers (utils) to use in new project
+
+
+## Installation
+
+```
+npm install --save xop-module-utils
+```
+
+
+## [Changelog](CHANGELOG)
+
+
+## [License](LICENSE)
---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * Utils
+ * v0.1.0
+ */
+
+
+/**
+ * Returns first element that matches CSS selector {expr}.
+ * Querying can optionally be restricted to {container}’s descendants
+ *
+ * @param expr
+ * @param container
+ * @returns {Node}
+ */
+export function find(expr, container) {
+    return typeof expr === 'string' ? (container || document).querySelector(expr) : expr || null;
+}
+
+
+/**
+ * Returns all elements that match CSS selector {expr} as an array.
+ * Querying can optionally be restricted to {container}’s descendants
+ *
+ * @param expr
+ * @param container
+ * @returns {*|Array}
+ */
+export function findAll(expr, container) {
+    return [].slice.call((container || document).querySelectorAll(expr));
+}
+
+
+/**
+ * Serialize object
+ * @param obj
+ * @returns {string}
+ */
+export function serialize(obj) {
+    let query = [];
+
+    for (let p in obj)
+        if (obj.hasOwnProperty(p)) {
+            query.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+        }
+
+    return query.join('&');
+}
